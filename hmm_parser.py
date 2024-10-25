c909ef73b2215c5fa2f85636ebeb00f381a55fc3
abdealijivaji <abujivaji@gmail.com>
2024-10-25 00:04:32 -0400

    basic framework done

--- hmm_parser.py
+++ hmm_parser.py
@@ -2,12 +2,11 @@
 import sys, os, re, shlex, subprocess, pandas, argparse
 from collections import defaultdict
 
-#print(sys.path)
 file = "test/ERX288947.23.dc.fltr.hmmout"
 
 def parse_hmmout(hmmout):
     input = open(hmmout, "r")
-    split = []
+    allrows = []
     for i in input.readlines():
         line = i.rstrip()
         if line.startswith("#"):
@@ -17,13 +16,24 @@ def parse_hmmout(hmmout):
             #split = []
             #split = line.split(" ", maxsplit= 19)
             
-            split += re.split(r"\s+", line, maxsplit=18) # change number for different files
-            #print(split)
-        
-    return split
+            split = re.split(r"\s+", line, maxsplit=18) # change number for different files
+            row = "\t".join(split)
+            #print(row)
+            
+            allrows.append(row)
+    return allrows 
 #print(newline)
+colnames = ['target_name', 'target_accession', 'query_name', 'query_accession',
+                       'evalue', 'score', 'bias',
+                       'dom_evalue', 'dom_score', 'dom_bias',
+                       'exp', 'reg', 'clu', 'ov', 'env', 'dom', 'rep', 'inc', 'description_of_target']
+cols = ["\t".join(colnames)]
+#print(colnames)
 
-#return newline
-
-
-print(parse_hmmout(file))
+#parse_hmmout(file)
+#print(parse_hmmout(file))
+df1 = pandas.DataFrame(cols)
+df2 = pandas.DataFrame(parse_hmmout(file))
+#print(df)
+df = pandas.concat([df1, df2])
+df.to_csv("./test.txt", index=False, header=False)
